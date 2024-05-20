303563d05a74b22992b1e4bf038bdc439051bd54
S_Santoshi <s.santoshi@shorelineiot.com>
2024-05-20 08:27:57 +0530

    helper function complete and frontend end folder added

--- backend/app.py
+++ backend/app.py
@@ -2,8 +2,9 @@ import os
 from flask import Flask, flash, request, redirect, url_for
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
-from helper import unzip_resumes,get_text,get_jd_text,get_embedding,get_score
-from LLM_module import get_question
+from helper import unzip_resumes,get_text,preprocess_text,get_jd_text,get_embedding,get_question,get_score
+import warnings
+warnings.filterwarnings("ignore")
 
 # UPLOAD_FOLDER = '/path/to/the/uploads'
 # ALLOWED_EXTENSIONS = {'txt', 'pdf'}
@@ -13,23 +14,30 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 app = Flask(__name__)
 
-@app.route('/upload_pdf', methods=['POST'])
+@app.route('/upload_file', methods=['POST'])
 def upload_pdf():
-
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'}), 400
-    file = request.files['file']
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-    unzip_resumes("zip")
-    res_text=get_text("filename")
-    jd_text=get_jd_text("filename")
-    res_emb=get_embedding(res_text)
-    jd_emb=get_embedding(jd_text)
-    score=get_score(res_emb,jd_emb)
-    question=get_question(res_text)
-    output=0
-    return output
+    output=[]
+    # if 'file' not in request.files:
+    #     return jsonify({'error': 'No file part'}), 400
+    # file = request.files['file']
+    # if file.filename == '':
+    #     return jsonify({'error': 'No selected file'}), 400
+    unzip_resumes() # from frontend
+    jd_text=get_jd_text("job_desc/jd_qa_sr.pdf") # get_name from frontend
+    jd_res=preprocess_text(jd_text)
+    jd_emb=get_embedding(jd_res)
+    for filename in os.listdir("resumes/"):
+        file_path = os.path.join("resumes/", filename)
+        if os.path.isfile(file_path):
+            print(filename)
+        res_text=get_text(file_path)
+        res_res=preprocess_text(res_text)
+        question=get_question(res_text)
+        res_emb=get_embedding(res_res)
+        score=get_score(res_emb,jd_emb)
+        output.append({"name":filename,"questions":question,"score":score,"path":file_path})
+        sorted_output = sorted(output, key=lambda x: x['score'], reverse=True)
+    return sorted_output
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/helper.py
+++ backend/helper.py
@@ -1,37 +1,56 @@
 import spacy
-from spacy.tokens import Doc
-# from spacy.lang.en.stop_words import STOP_WORDS
-# import nltk
-from gensim.models import Word2Vec, Doc2Vec
+from gensim.models import Doc2Vec
 from sklearn.metrics.pairwise import cosine_similarity
-# nltk.download('punkt')
+from pypdf import PdfReader
+import google.generativeai as genai
+import shutil
+import zipfile
+import os
 
-from nltk.tokenize import word_tokenize
+GOOGLE_API_KEY='AIzaSyBIcnlFK8DcWQFFDm_Axw5OevHwmKxK2VM'
+# nltk.download('punkt')
+loaded_model = Doc2Vec.load("doc2vec_model")
 nlp = spacy.load("en_core_web_md")
 
-print("Started")
-from pypdf import PdfReader
-reader = PdfReader('sample_resume/resume40.pdf') 
-page = reader.pages[0] 
-resume_text = page.extract_text() 
+def unzip_resumes():
+    if os.path.exists("resumes"):
+        shutil.rmtree("resumes")
+    os.makedirs("resumes", exist_ok=True)
+    with zipfile.ZipFile("resumezip.zip", 'r') as zip_ref:
+        zip_ref.extractall("resumes")
+        
+def get_text(filename):
+    reader = PdfReader(filename) 
+    page = reader.pages[0] 
+    resume_text = page.extract_text() 
+    return resume_text
 
-reader = PdfReader('sample_resume/jd_fd_jr.pdf') 
-page = reader.pages[0] 
-jd_text = page.extract_text() 
-start_pos = jd_text.find("Requirements:")
-jd_text = jd_text[start_pos:]
-print("Text Recieved")
+def get_jd_text(filename):
+    reader = PdfReader(filename) 
+    page = reader.pages[0] 
+    jd_text = page.extract_text() 
+    start_pos = jd_text.find("Requirements:")
+    jd_text = jd_text[start_pos:]
+    return jd_text
+
 
 def preprocess_text(text):
     doc = nlp(text)
     tokens = [token.lemma_ for token in doc if not token.is_stop and not token.is_punct]
     return tokens
 
-preprocessed_resume = preprocess_text(resume_text)
-preprocessed_jd = preprocess_text(jd_text)
-loaded_model = Doc2Vec.load("doc2vec_model")
 
-resume_doc_embedding = loaded_model.infer_vector(preprocessed_resume)
-jd_doc_embedding = loaded_model.infer_vector(preprocessed_jd)
-doc_similarity_score = cosine_similarity(resume_doc_embedding.reshape(1, -1), jd_doc_embedding.reshape(1, -1))[0][0]
-print("Cosine Similarity Score (Doc Embeddings):", doc_similarity_score)
+def get_embedding(tokens):
+    emb = loaded_model.infer_vector(tokens)
+    return emb
+
+def get_score(res,jd):
+    score = (cosine_similarity(res.reshape(1, -1), jd.reshape(1, -1))[0][0])*100
+    return round(score, 2)
+
+def get_question(res_text):
+    genai.configure(api_key=GOOGLE_API_KEY)
+    model = genai.GenerativeModel('gemini-pro')
+    prompt=f"The following is a resume of a candidate \n {res_text} \n. You are an HR. Generate 5 follow-up questions based on this resume. Rephrase the questions and make them shorter. Do not add any text effects or aestricks in the response"
+    response = model.generate_content(prompt)
+    return response.text
